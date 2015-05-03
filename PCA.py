30d7fc8b472bf242977a6f689ce8fbd1f2a39353
Tejas Khot <tjskhot@gmail.com>
2015-05-03 14:03:20 +0530

    Initial code

---
+++ PCA.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+np.random.seed(234234782384239784) # random seed for consistency
+
+mu_vec1 = np.array([0,0,0])
+cov_mat1 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class1_sample = np.random.multivariate_normal(mu_vec1, cov_mat1, 20).T
+assert class1_sample.shape == (3,20), "The matrix does not have the dimensions 3x20"
